e7aeeb21569770016954a5c97bb6078cf938a5c5
Eric Hernandez <eric.hernandez.447@my.csun.edu>
2016-11-22 21:07:41 -0800

    Add files via upload
    Script to install apache, php, phpmyadmin, and mysql. Plus creating db, table, and columns

---
+++ phaseII/424setup.sh
@@ -0,0 +1,20 @@
+# to run do: source p.sh
+
+sudo apt-get update
+sudo apt-get install apache2
+sudo apt-get install mysql-server
+sudo apt-get install php5
+sudo apt-get install phpmyadmin
+
+#sudo chmod -R 777 /var/www/html/ #this works! to access /var/www/html/
+
+#sudo echo "<?php phpinfo(); ?>" >> /var/www/html/info.php
+
+#sudo chmod -R 644 /var/www/html #this gets rid of permission on /var/www/html/
+
+#sudo echo "hello world man guy" >> /var/www/html/what.txt
+
+#chmod +x /var/www/html
+echo "++INPUT MYSQL PASSWORD++"
+
+mysql -u root -p -e "drop database 424Proj2; create database 424Proj2; use 424Proj2; create table users (id int not null auto_increment, primary key (id), username varchar(255), email varchar(255), password varchar(255), loginCount int(100), firstName varchar(255), lastName varchar(255) ); describe users;"
