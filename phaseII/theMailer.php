d7cf2ea127a7e1d6db7567a0b0cdf47e9186eb29
iuc73663 <ivan5247@gmail.com>
2016-11-27 00:52:22 -0800

    added mailer function; included dummy email

---
+++ phaseII/theMailer.php
@@ -0,0 +1,23 @@
+
+
+<?php
+require 'PHPMailer/PHPMailerAutoload.php';
+function sendMail($to, $subject, $body) {
+	$mail = new PHPMailer(); // create a new object
+	$mail->IsSMTP(); // enable SMTP
+	//$mail->SMTPDebug = 1; // debugging: 1 = errors and messages, 2 = messages only
+	$mail->SMTPAuth = true; // authentication enabled
+	$mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for Gmail
+	$mail->Host = "smtp.gmail.com";
+	$mail->Port = 465; // or 587
+	$mail->IsHTML(true);
+	$mail->Username = "comp424testing@gmail.com";
+	$mail->Password = "tryingtosurvive";
+	$mail->SetFrom("ivan5247@gmail.com");
+	$mail->Subject = $subject;
+	$mail->Body = $body;
+	$mail->AddAddress($to);
+	return $mail->send();
+}
+
+?>
